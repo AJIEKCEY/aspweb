296b74e1a11d8303fcfeb681f0a1309a4e9115fc
AJIEKCEY <sergheialexei@gmail.com>
2021-05-10 23:25:52 +0300

    Initial comit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace aspweb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
